d187547e5dd6940f21336dc69dc942c1300c429c
Gleen Oinometse <135310677+Oinometse@users.noreply.github.com>
2024-04-05 19:14:07 +0800

    Update page.tsx
    remove dot in h1 headline

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <Image src={`${process.env.APP_S3_BUCKET}/assets/Home-Renovations.webp`} fill={true} priority={true} alt="Maiker House Project Footer Logo" className="absolute top-0 object-cover h-[inherit]"/>
         <div className="z-10 relative max-w-[1250px] mx-auto mt-[150px] sm:mt-[200px]">
           <div className="w-full sm:w-[600px] px-5 sm:px-0">
-            <h1 className="font-[800] mb-10 text-[30px] sm:text-[38px]">High quality contemporary renovation builder to bring your home to life.</h1>
+            <h1 className="font-[800] mb-10 text-[30px] sm:text-[38px]">High quality contemporary renovation builder to bring your home to life</h1>
             <p className="font-[500] text-[18px] mb-10">Maiker Constructions designs and builds custom home renovations and additions projects. We specialize in timber construction and the modernisation of character homes, offering a comprehensive landscape and hardscape solution.</p>
             <ScrollButtonHome targetSectionId="footer"/>
             <div className="w-full">
